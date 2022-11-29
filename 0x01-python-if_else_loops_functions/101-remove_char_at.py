21b106ab0fbade177916b325ddd866da366bd2a4
Irnes <nonhlanhladube05@gmail.com>
2022-11-29 15:11:47 +0000

    task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-# 100-print_tebahpla.py
+# 101-remove_char_at.py
 # Brennan D Baraban <375@holbertonschool.com>
 
-""""Print the alphabet in reverse order alternating upper- and lower-case."""
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+
+def remove_char_at(str, n):
+    """Create a copy of the string without the character at position n."""
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
